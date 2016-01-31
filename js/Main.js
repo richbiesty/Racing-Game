6c0b40a986a0d965d220311adbc850a7f1848896
Richard Biesty <richiebiesty@gmail.com>
2016-01-31 19:50:11 +0000

    added js

---
+++ js/Main.js
@@ -0,0 +1,49 @@
+var canvas, canvasContext;
+
+var blueCar = new carClass();
+var greenCar = new carClass();
+
+window.onload = function() {
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+	colorText("LOADING IMAGES", canvas.width/2,canvas.height/2, 'white');
+
+	loadImages();
+}
+
+function imageLoadingDoneSoStartGame() {
+	var framesPerSecond = 30;
+	setInterval(updateAll, 1000/framesPerSecond);
+
+	setupInput();
+
+	loadLevel(levelOne);
+}
+
+function loadLevel(whichLevel) {
+	trackGrid = whichLevel.slice();
+	greenCar.reset(otherCarPic, "Green Car");
+	blueCar.reset(carPic, "Blue Car");
+}
+
+function updateAll() {
+	moveAll();
+	drawAll();
+}
+
+function moveAll() {
+	blueCar.move();
+	greenCar.move();
+}
+
+function clearScreen() {
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+}
+
+function drawAll() {
+	drawTracks();
+	greenCar.draw();
+	blueCar.draw();
+} 
